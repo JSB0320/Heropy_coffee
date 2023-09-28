a45067cf91e8511f8c6bd77433af6cb18ae1f987
SuBinJung <banyeong0320@gmail.com>
2023-09-28 17:03:27 +0900

    섹션 애니메이션 구현, 푸터 및 최상단 버튼 추가_마무리

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener('blur', function(){
 });
 //페이지 스크롤에 따른 요소 제어
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', function(){
   console.log(window.scrollY);
@@ -26,14 +27,29 @@ window.addEventListener('scroll', function(){
       opcity: 0,
       display: 'none'
     });
+    // 상단으로 이동 버튼 보이기!
+    gsap.to(toTopEl, .6, {
+      opacity: 1,
+      x: 0
+    })
   } else{
     // badge 요소 보이기!
     gsap.to(badgeEl, .6, {
       opcity: 1,
       display: 'block'
     });
+    // 상단으로 이동 버튼 숨기기!
+    gsap.to(toTopEl, .6, {
+      opacity: 0,
+      x: 100
+    })
   }
 })
+toTopEl.addEventListener('click', function(){
+  gsap.to(window, .6, {
+    scrollTo: 0
+  })
+});
 
 // 나타날 요소(.fade-in)들을 찾기
 const fadeEls = document.querySelectorAll('.visual .fade-in');
@@ -46,36 +62,54 @@ fadeEls.forEach(function(fadeEl, index){
 });
 //슬라이드(swiper)
 new Swiper('.notice .swiper', {
-  direction: 'vertical', //수직 슬라이드
-  autoplay: true, //자동 재생 여부
-  loop: true //반복 재생 여부
+  direction: 'vertical', // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부
 });
 new Swiper('.promotion .swiper', {
-  //direction: 'horizontal', //수평 슬라이드
-  autoplay: true, //자동 재생 여부
-  loop: true, //반복 재생 여부
-  slidesPerView: 3, //한 번에 보여줄 슬라이드 개수
-  spaceBetween: 10, //슬라이드 사이 여백
-  centeredSlides: true, //1번 슬라이드가 가운데 보이기
-  pagination: { //페이지 번호 사용
-    el: '.promotion .swiper-pagination', //페이지 번호 요소
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: { // 자동 재생 여부
+    delay: 5000 // 5초마다 슬라이드 바뀜
+  },
+  loop: true, // 반복 재생 여부
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  pagination: { // 페이지 번호 사용
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소
     clickable: true // 사용자의 페이지 번호 제어 여부
   },
   navigation: { // 슬라이드 이전/다음 버튼 사용
     prevEl: '.promotion .swiper-button-prev', // 이전 버튼 요소
-    nextEl: '.promotion .swiper-button-next', // 다음 버튼 요소
+    nextEl: '.promotion .swiper-button-next' // 다음 버튼 요소
   }
 });
+new Swiper('.awards .swiper', {
+ autoplay: true,
+ loop: true,
+ spaceBetween: 30,
+ slidesPerView: 5,
+ navigation: {
+  prevEl: '.awards .swiper-button-prev',
+  nextEl: '.awards .swiper-button-next'
+ }
+});
+
+
+// 슬라이드 영역 요소 검색!
 const promotionEl = document.querySelector('section.promotion');
-const promotionToggleBtn = document.querySelector('.toggle-promotion')
-//토글 버튼을 클릭했을 때.
-promotionToggleBtn.addEventListener('click', function(){
-  if(promotionEl.classList.contains('hide')) {
+// 슬라이드 영역를 토글하는 버튼 검색!
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+
+// 토글 버튼을 클릭하면,
+promotionToggleBtn.addEventListener('click', function () {
+  if (promotionEl.classList.contains('hide')) {
     promotionEl.classList.remove('hide');
   } else {
     promotionEl.classList.add('hide');
   }
-})
+});
+
 gsap.to('.floating1', 1.5, {
   delay: 1, //얼마나 늦게 애니메이션을 시작할 것인지 지연시간을 설정
   y: 15, //'transform: translateY(수치);와 같음. 수직으로 얼마나 움직일지 설정
@@ -89,7 +123,7 @@ gsap.to('.floating2', 2, {
   repeat: -1,
   yoyo: true,
   ease: Power1.easeInOut 
-})
+})            
 gsap.to('.floating3', 2.5, {
   delay: 1.5,
   y: 20,
@@ -108,3 +142,7 @@ spyEls.forEach(function(spyEl){
   .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
   .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
 });
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
